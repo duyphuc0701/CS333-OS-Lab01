a33bcf41af6c908b74731c624b463d89cab4f5ed
duyphuc0701 <duyphuc070104@gmail.com>
2025-01-19 15:39:39 +0700

    Rename variables for find.c

--- user/find.c
+++ user/find.c
@@ -4,71 +4,71 @@
 #include "kernel/fs.h"
 
 // Function to extract the base name (file or directory name) from a given path
-char *fmtname(char *path) {
-    char *p;
-    for (p = path + strlen(path); p >= path && *p != '/'; p--);
-    return p + 1;
+char *extractBaseName(char *path) {
+    char *lastSlash;
+    for (lastSlash = path + strlen(path); lastSlash >= path && *lastSlash != '/'; lastSlash--);
+    return lastSlash + 1;
 }
 
-// Recursive function to find the file
-void find(char *path, char *file) {
-    char buf[512], *p;
-    int fd;
-    struct dirent de;
-    struct stat st;
+// Recursive function to search for the file
+void searchFile(char *directoryPath, char *targetFileName) {
+    char pathBuffer[512], *pathPointer;
+    int directoryFd;
+    struct dirent directoryEntry;
+    struct stat directoryStats;
 
     // Open the directory
-    if ((fd = open(path, 0)) < 0) {
-        fprintf(2, "find: cannot open %s\n", path);
+    if ((directoryFd = open(directoryPath, 0)) < 0) {
+        fprintf(2, "find: cannot open %s\n", directoryPath);
         return;
     }
 
-    // Get the metadata of the path
-    if (fstat(fd, &st) < 0) {
-        fprintf(2, "find: cannot stat %s\n", path);
-        close(fd);
+    // Get the metadata of the directory
+    if (fstat(directoryFd, &directoryStats) < 0) {
+        fprintf(2, "find: cannot stat %s\n", directoryPath);
+        close(directoryFd);
         return;
     }
 
-    switch (st.type) {
+    switch (directoryStats.type) {
     case T_FILE:
-        if (strcmp(fmtname(path), file) == 0) {
-            printf("%s\n", path);
+        if (strcmp(extractBaseName(directoryPath), targetFileName) == 0) {
+            printf("%s\n", directoryPath);
         }
         break;
 
     case T_DIR:
         // Ensure the buffer has enough space for the new path
-        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)) {
+        if (strlen(directoryPath) + 1 + DIRSIZ + 1 > sizeof(pathBuffer)) {
             fprintf(2, "find: path too long\n");
             break;
         }
 
         // Copy the directory path to the buffer
-        strcpy(buf, path);
-        p = buf + strlen(buf);
-        *p++ = '/';
+        strcpy(pathBuffer, directoryPath);
+        pathPointer = pathBuffer + strlen(pathBuffer);
+        *pathPointer++ = '/';
 
         // Read directory entries
-        while (read(fd, &de, sizeof(de)) == sizeof(de)) {
-            if (de.inum == 0)
+        while (read(directoryFd, &directoryEntry, sizeof(directoryEntry)) == sizeof(directoryEntry)) {
+            if (directoryEntry.inum == 0)
                 continue;
 
             // Skip "." and ".." to avoid infinite loops
-            if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+            if (strcmp(directoryEntry.name, ".") == 0 || strcmp(directoryEntry.name, "..") == 0)
                 continue;
 
             // Append the entry name to the path
-            memmove(p, de.name, strlen(de.name));
-            p[strlen(de.name)] = '\0';
+            memmove(pathPointer, directoryEntry.name, strlen(directoryEntry.name));
+            pathPointer[strlen(directoryEntry.name)] = '\0';
 
-            // Recursively call find for the new path
-            find(buf, file);
+            // Recursively call searchFile for the new path
+            searchFile(pathBuffer, targetFileName);
         }
         break;
     }
 
-    close(fd);
+    close(directoryFd);
 }
 
 int main(int argc, char *argv[]) {
@@ -78,10 +78,10 @@ int main(int argc, char *argv[]) {
     }
 
     if (argc == 2) {
-        find(".", argv[1]); // Default to current directory if no directory is provided
+        searchFile(".", argv[1]); // Default to current directory if no directory is provided
     } else {
         for (int i = 1; i < argc - 1; i++) {
-            find(argv[i], argv[argc - 1]); // Search in specified directories
+            searchFile(argv[i], argv[argc - 1]); // Search in specified directories
         }
     }
 
